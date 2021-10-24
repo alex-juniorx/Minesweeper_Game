4ba69e9af1e62434fd0a60a55bd85e8575e1c53e
Alex Junior <alex_juniorx@yahoo.com.br>
2021-10-23 22:01:47 -0300

    Created the initial game board ans started defining the bomb positions.

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,8 +1,29 @@
+# MINESWEEPER GAME INTRODUCTION:
 print("__________________________________")
 print(" ")
 print("WELCOME TO THE MINESWEEPER GAME!")
 print("__________________________________")
-# MINESWEEPER GAME INTRODUCTION
 
+from random import randint
+
+# Creating a null board game:
+board = []
+for i in range(9):
+    board.append(["O"]*9)
+
+# Function used to print the actual game board:
+def print_board(board):
+    for row in board:
+        print (" ".join(row))
+
+print_board(board)
+
+# Function to generate the random positions [row, column] to the bombs:
+def random_position(board):
+    return [randint(0, len(board)-1), randint(0, len(board)-1)]
+
+bomb1 = random_position(board)
+
+print(bomb1)
 
 
