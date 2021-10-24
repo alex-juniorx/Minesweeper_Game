c6bec94f2da530135d367b0318fcfdf9d290ad58
Alex Junior <alex_juniorx@yahoo.com.br>
2021-10-24 13:03:01 -0300

    Defined bomb positions and started to define the solution board

--- Minesweeper.py
+++ Minesweeper.py
@@ -3,6 +3,7 @@ print("__________________________________")
 print(" ")
 print("WELCOME TO THE MINESWEEPER GAME!")
 print("__________________________________")
+print(" ")
 
 from random import randint
 
@@ -11,10 +12,17 @@ board = []
 for i in range(9):
     board.append(["O"]*9)
 
-# Function used to print the actual game board:
-def print_board(board):
-    for row in board:
-        print (" ".join(row))
+# Function used to print the actual game board (with numbered rows and columns):
+def print_board(given_board):
+    # Numbering the columns:
+    print("   0  1  2  3  4  5  6  7  8")
+    print("   _  _  _  _  _  _  _  _  _")
+    # count used only to number the rows:
+    count = 0
+    for row in given_board:
+        print(count, end=" |"),
+        print ("  ".join(row))
+        count += 1
 
 print_board(board)
 
@@ -22,8 +30,39 @@ print_board(board)
 def random_position(board):
     return [randint(0, len(board)-1), randint(0, len(board)-1)]
 
-bomb1 = random_position(board)
+# Generating the position of the ten bombs (ensuring non-duplicate values):
+bombs_position = []
+for i in range(10):
+    temp_position = random_position(board)
+    while temp_position in bombs_position:
+        temp_position = random_position(board)
+    bombs_position.append(temp_position)
 
-print(bomb1)
+# Creating the solution board, which will be hidden from the player:
+solution = []
+for i in range(9):
+    solution.append(["_"]*9)
+for bomb in bombs_position:
+    solution[bomb[0]][bomb[1]] = "X"
+for row in solution:
+    for column in row:
+
+print_board(solution)
+
+
+
+
+
+
+
+
+
+
+# Starting the game and initiating an interactive loop:
+
+
+
+
+
 
 
